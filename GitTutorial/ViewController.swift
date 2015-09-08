c71091b2eacea656476e7728764ca4e2ad387295
Anand <anand.letap@gmail.com>
2015-09-08 16:01:49 -0400

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Anand on 9/8/15.
+//  Copyright (c) 2015 AnandPatel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        println("Hello World!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
